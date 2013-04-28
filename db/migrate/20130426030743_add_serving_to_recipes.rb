20371a4fe4d0aff85f8be900046f35763db2e561
Linh Bui <linhdbuivn@gmail.com>
2013-04-27 22:07:37 -0400

    add serving to recipes

--- db/migrate/20130426030743_add_serving_to_recipes.rb
+++ db/migrate/20130426030743_add_serving_to_recipes.rb
@@ -1,6 +1,6 @@
 class AddServingToRecipes < ActiveRecord::Migration
   def self.up
-    add_column :recipes, :serving, :decimal
+    add_column :recipes, :serving, :decimal, default: 4
   end
 
   def self.down
